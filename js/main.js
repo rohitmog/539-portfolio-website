3b57943c18f4000d88cf01db8196895d9b997a4e
rohitmog <rohitmog@umich.edu>
2019-02-05 23:11:48 -0500

    Added Loading Page

--- js/main.js
+++ js/main.js
@@ -1,5 +1,8 @@
 /*global $, jQuery, alert*/
 $(document).ready(function() {
+    setTimeout(function() {
+        $('body').addClass('loaded');
+    }, 1000);
 
   'use strict';
 
@@ -47,29 +50,9 @@ $(document).ready(function() {
   }
 
   // ========================================================================= //
-  //  //NAVBAR SHOW - HIDE
+  //  // LOADING PAGE
   // ========================================================================= //
 
-
-  $(window).scroll(function() {
-    var scroll = $(window).scrollTop();
-    if (scroll > 200 ) {
-      $("#main-nav, #main-nav-subpage").slideDown(700);
-      $("#main-nav-subpage").removeClass('subpage-nav');
-    } else {
-      $("#main-nav").slideUp(700);
-      $("#main-nav-subpage").hide();
-      $("#main-nav-subpage").addClass('subpage-nav');
-    }
-  });
-
-  // ========================================================================= //
-  //  // RESPONSIVE MENU
-  // ========================================================================= //
-
-  $('.responsive').on('click', function(e) {
-    $('.nav-menu').slideToggle();
-  });
   // ========================================================================= //
   //  // Text Scramble
   // ========================================================================= //
@@ -90,60 +73,6 @@ class TextScramble {
       const to = newText[i] || ''
       const start = Math.floor(Math.random() * 40)
       const end = start + Math.floor(Math.random() * 100)
-      this.queue.push({ from, to, start, end })
-    }
-    cancelAnimationFrame(this.frameRequest)
-    this.frame = 0
-    this.update()
-    return promise
-  }
-  update() {
-    let output = ''
-    let complete = 0
-    for (let i = 0, n = this.queue.length; i < n; i++) {
-      let { from, to, start, end, char } = this.queue[i]
-      if (this.frame >= end) {
-        complete++
-        output += to
-      } else if (this.frame >= start) {
-        if (!char || Math.random() < 0.28) {
-          char = this.randomChar()
-          this.queue[i].char = char
-        }
-        output += `<span class="dud">${char}</span>`
-      } else {
-        output += from
-      }
-    }
-    this.el.innerHTML = output
-    if (complete === this.queue.length) {
-      this.resolve()
-    } else {
-      this.frameRequest = requestAnimationFrame(this.update)
-      this.frame++
-    }
-  }
-  randomChar() {
-    return this.chars[Math.floor(Math.random() * this.chars.length)]
-  }
-}
-
-class TextScrambleNumbers {
-  constructor(el) {
-    this.el = el
-    this.chars = '042697'
-    this.update = this.update.bind(this)
-  }
-  setText(newText) {
-    const oldText = this.el.innerText
-    const length = Math.max(oldText.length, newText.length)
-    const promise = new Promise((resolve) => this.resolve = resolve)
-    this.queue = []
-    for (let i = 0; i < length; i++) {
-      const from = oldText[i] || ''
-      const to = newText[i] || ''
-      const start = Math.floor(Math.random() * 40)
-      const end = start + Math.floor(Math.random() * 250)
       this.queue.push({ from, to, start, end })
     }
     cancelAnimationFrame(this.frameRequest)
@@ -260,103 +189,10 @@ const next33 = () => {
 }
 
 next33()
-// // ——————————————————————————————————————————————————
-// // Age Text Scramble
-// // ——————————————————————————————————————————————————
-
-const phrases4 = ["22"]
-
-const el4 = document.querySelector('.age')
-const fx4 = new TextScrambleNumbers(el4)
-
-let counter4 = 0
-const next4 = () => {
-  fx4.setText(phrases4[counter4])
-  counter4 = (counter4 + 1) % phrases4.length
-}
-
-next4()
-// // ——————————————————————————————————————————————————
-// // Projects Text Scramble
-// // ——————————————————————————————————————————————————
-
-const phrases5 = ["10"]
-
-const el5 = document.querySelector('.projects')
-const fx5 = new TextScrambleNumbers(el5)
-
-let counter5 = 0
-const next5 = () => {
-  fx5.setText(phrases5[counter5])
-  counter5 = (counter5 + 1) % phrases5.length
-}
-
-next5()
-// // ——————————————————————————————————————————————————
-// // Services Text Scramble
-// // ——————————————————————————————————————————————————
-
-const phrases6 = ["3"]
-
-const el6 = document.querySelector('.services')
-const fx6 = new TextScrambleNumbers(el6)
-
-let counter6 = 0
-const next6 = () => {
-  fx6.setText(phrases6[counter6])
-  counter6 = (counter6 + 1) % phrases6.length
-}
 
 next6()
-  // ========================================================================= //
-  //  Typed Js
-  // ========================================================================= //
 
-
-  var typed = $(".typed");
-
-  $(function() {
-    typed.typed({
-      strings: ["Sports Analytics Consultant"] ,
-      typeSpeed: 100
-
-    });
-  });
-  
-  var typedSchool = $(".typedSchool");
-
-  $(function() {
-    typedSchool.typed({
-      strings: ["University of Michigan"] ,
-      typeSpeed: 100
-
-    });
-  });
-
- var typedAge = $(".typedAge");
-
-  $(function() {
-    typedAge.typed({
-      strings: ["1","2","3","4","5","6","7","8","9","10",
-                "11","12","13","14","15","16","17","18","19","20","22"],
-      typeSpeed: 0,
-    });
-  });
-  // ========================================================================= //
-  //  Owl Carousel Services
-  // ========================================================================= //
-
-
-  $('.services-carousel').owlCarousel({
-      autoplay: true,
-      loop: true,
-      margin: 20,
-      dots: true,
-      nav: false,
-      responsiveClass: true,
-      responsive: { 0: { items: 1 }, 768: { items: 2 }, 900: { items: 4 } }
-    });
-
+ 
 
   // ========================================================================= //
   //  Porfolio isotope and filter
@@ -381,6 +217,20 @@ $(".hover").mouseleave(
   }
 );
 
+$(".btn").click(function() {
+  
+   var lable = $(".btn").text().trim();
+
+   if(lable == "Hide") {
+     $(".btn").text("Show");
+     $(".myText").hide();
+   }
+   else {
+     $(".btn").text("Hide");
+     $(".myText").show();
+   }
+    
+  });
 
   // ========================================================================= //
   //  magnificPopup
