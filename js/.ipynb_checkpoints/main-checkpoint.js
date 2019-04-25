f0a0673dad9f796deacfbcd58ad5a6591d153bcc
rohitmog <rohitmog@umich.edu>
2019-04-24 21:05:00 -0400

    Added images of size (6.5, 4.57) to the website

---
+++ js/.ipynb_checkpoints/main-checkpoint.js
@@ -0,0 +1,233 @@
+/*global $, jQuery, alert*/
+$(document).ready(function() {
+    setTimeout(function() {
+        $('body').addClass('loaded');
+    }, 1000);
+
+  'use strict';
+
+  // ========================================================================= //
+  //  //SMOOTH SCROLL
+  // ========================================================================= //
+
+
+  $(document).on("scroll", onScroll);
+
+  $('a[href^="#"]').on('click', function(e) {
+    e.preventDefault();
+    $(document).off("scroll");
+
+    $('a').each(function() {
+      $(this).removeClass('active');
+      if ($(window).width() < 768) {
+        $('.nav-menu').slideUp();
+      }
+    });
+
+    $(this).addClass('active');
+
+    var target = this.hash,
+        menu = target;
+
+    target = $(target);
+    $('html, body').stop().animate({
+      'scrollTop': target.offset().top - 80
+    }, 500, 'swing', function() {
+      window.location.hash = target.selector;
+      $(document).on("scroll", onScroll);
+    });
+  });
+
+
+  function onScroll(event) {
+    if ($('.home').length) {
+      var scrollPos = $(document).scrollTop();
+      $('nav ul li a').each(function() {
+        var currLink = $(this);
+        var refElement = $(currLink.attr("href"));
+      });
+    }
+  }
+
+  // ========================================================================= //
+  //  // Navigation Bar
+  // ========================================================================= //
+class StickyNavigation {
+  
+  constructor() {
+    this.currentId = null;
+    this.currentTab = null;
+    this.tabContainerHeight = 70;
+    let self = this;
+    $('.et-hero-tab').click(function() { 
+      self.onTabClick(event, $(this)); 
+    });
+    $(window).scroll(() => { this.onScroll(); });
+    $(window).resize(() => { this.onResize(); });
+  }
+  
+  onTabClick(event, element) {
+    event.preventDefault();
+    let scrollTop = $(element.attr('href')).offset().top - this.tabContainerHeight - 30;
+    $('html, body').animate({ scrollTop: scrollTop }, 600);
+  }
+  
+  onScroll() {
+    this.checkTabContainerPosition();
+    this.findCurrentTabSelector();
+  }
+  
+  onResize() {
+    if(this.currentId) {
+      this.setSliderCss();
+    }
+  }
+  
+  checkTabContainerPosition() {
+    let offset = $('.et-hero-tabs').offset().top + $('.et-hero-tabs').height() - this.tabContainerHeight;
+    if($(window).scrollTop() > offset) {
+      $('.et-hero-tabs-container').addClass('et-hero-tabs-container--top');
+    } 
+    else {
+      $('.et-hero-tabs-container').removeClass('et-hero-tabs-container--top');
+    }
+  }
+  
+  findCurrentTabSelector(element) {
+    let newCurrentId;
+    let newCurrentTab;
+    let self = this;
+    $('.et-hero-tab').each(function() {
+      let id = $(this).attr('href');
+      let offsetTop = $(id).offset().top - self.tabContainerHeight;
+      let offsetBottom = $(id).offset().top + $(id).height() - self.tabContainerHeight;
+      if($(window).scrollTop() > offsetTop && $(window).scrollTop() < offsetBottom) {
+        newCurrentId = id;
+        newCurrentTab = $(this);
+      }
+    });
+    if(this.currentId != newCurrentId || this.currentId === null) {
+      this.currentId = newCurrentId;
+      this.currentTab = newCurrentTab;
+      this.setSliderCss();
+    }
+  }
+  
+  setSliderCss() {
+    let width = 0;
+    let left = 0;
+    if(this.currentTab) {
+      width = this.currentTab.css('width');
+      left = this.currentTab.offset().left;
+    }
+    $('.et-hero-tab-slider').css('width', width);
+    $('.et-hero-tab-slider').css('left', left);
+  }
+  
+}
+
+new StickyNavigation();
+  // ========================================================================= //
+  //  // Text Scramble
+  // ========================================================================= //
+    $("#typed1").typed({
+      strings: ["ROHIT MOGALAYAPALLI"],
+      typeSpeed: 100,
+      startDelay: 50,
+      cursorChar: " ",
+      contentType: 'html'
+    });
+  $("#typed2").typed({
+      strings: ["Sports Analytics Consultant"],
+      typeSpeed: 100,
+      startDelay: 50,
+      cursorChar: "",
+      contentType: 'html'
+    });
+    $("#typed3").typed({
+      strings: ["University of Michigan"],
+      typeSpeed: 100,
+      startDelay: 1000,
+      cursorChar: "",
+      contentType: 'html'
+    });
+      $("#typed4").typed({
+      strings: ["M.S. Information"],
+      typeSpeed: 50,
+      startDelay: 3000,
+      cursorChar: "",
+      contentType: 'html'
+    });
+  // ========================================================================= //
+  //  Porfolio isotope and filter
+  // ========================================================================= //
+
+
+  var portfolioIsotope = $('.portfolio-container').isotope({
+    itemSelector: '.portfolio-thumbnail',
+    layoutMode: 'fitRows'
+  });
+
+  $('#portfolio-flters li').on( 'click', function() {
+    $("#portfolio-flters li").removeClass('filter-active');
+    $(this).addClass('filter-active');
+
+    portfolioIsotope.isotope({ filter: $(this).data('filter') });
+  });
+
+$(".hover").mouseleave(
+  function () {
+    $(this).removeClass("hover");
+  }
+);
+
+$(".btn").click(function() {
+  
+   var lable = $(".btn").text().trim();
+
+   if(lable == "Hide") {
+     $(".btn").text("Show");
+     $(".myText").hide();
+   }
+   else {
+     $(".btn").text("Hide");
+     $(".myText").show();
+   }
+    
+  });
+
+  // ========================================================================= //
+  //  magnificPopup
+  // ========================================================================= //
+
+  var magnifPopup = function() {
+    $('.popup-img').magnificPopup({
+      type: 'image',
+      removalDelay: 300,
+      mainClass: 'mfp-with-zoom',
+      gallery: {
+        enabled: true
+      },
+      zoom: {
+        enabled: true, // By default it's false, so don't forget to enable it
+
+        duration: 300, // duration of the effect, in milliseconds
+        easing: 'ease-in-out', // CSS transition easing function
+
+        // The "opener" function should return the element from which popup will be zoomed in
+        // and to which popup will be scaled down
+        // By defailt it looks for an image tag:
+        opener: function(openerElement) {
+          // openerElement is the element on which popup was initialized, in this case its <a> tag
+          // you don't need to add "opener" option if this code matches your needs, it's defailt one.
+          return openerElement.is('img') ? openerElement : openerElement.find('img');
+        }
+      }
+    });
+  };
+
+
+  // Call the functions
+  magnifPopup();
+
+});
